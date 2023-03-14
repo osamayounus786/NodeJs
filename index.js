4a8a83b8d38f090370f04d5feead13935c805dd5
Osama Younus <osama.younus@blended.team>
2023-03-14 11:05:49 +0500

    module eports

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const add = require('./add')
-console.log("Hello from Osama 1")
+require('./hulk')
+require('./thor')
 
-const sum = add(3,7)
-const sum1 = add(10,3)
-
-
-console.log(sum)
-console.log(sum1)
