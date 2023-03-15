d7e9e98ef8733a2617861f0c94da504148f59fa1
Osama Younus <osama.younus@blended.team>
2023-03-15 12:26:11 +0500

    module.expor vs exports

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // require('./hulk')
 // require('./thor')
 
+// const superHero = require('./super-hero')
+// console.log(superHero.getName())
+// superHero.setName('Super Man')
+// console.log(superHero.getName())
+
+// const newSuperHero = require('./super-hero')
+// console.log(newSuperHero.getName());
+
+const math = require('./math');
+
+ const {add,subt} = math; // destructuring
+
+console.log(add(8,3));
+console.log(subt(8,3));
---
+++ math.js
@@ -0,0 +1,24 @@
+
+
+// module.exports.add =  (a,b) => {
+//     return a + b;
+// }
+// module.exports.subt = (a,b) =>{
+//     return a - b;
+// }
+
+//both are similar work same and provide the expected output 
+
+
+// diff between module.exports vs exports
+
+exports.add =  (a,b) => {
+    return a + b;
+}
+exports.subt = (a,b) =>{
+    return a - b;
+}
+// module.exports = {
+//     add,
+//     subt,
+// };
