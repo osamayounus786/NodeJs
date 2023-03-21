55f3f7b8810029e778b622f56508135fccfbf037
Osama Younus <osama.younus@blended.team>
2023-03-21 17:57:08 +0500

    json and  node --watch

--- index.js
+++ index.js
@@ -9,9 +9,15 @@
 // const newSuperHero = require('./super-hero')
 // console.log(newSuperHero.getName());
 
-const math = require('./math');
+// const math = require('./math');
 
- const {add,subt} = math; // destructuring
+//  const {add,subt} = math; // destructuring
 
-console.log(add(8,3));
-console.log(subt(8,3));
+// console.log(add(8,3));
+// console.log(subt(8,3));
+
+
+const data = require('./data.json');
+
+
+console.log(data);
