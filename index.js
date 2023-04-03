590094e9b75ed8f390a43d184d69740a544da6a7
Osama Younus <osama.younus@blended.team>
2023-04-03 15:53:57 +0500

    json response

--- index.js
+++ index.js
@@ -134,8 +134,13 @@
 const http = require("node:http");
 
 const server =  http.createServer((req,res)=>{
-            res.writeHead(200, {"content-type": "text/plain" });
-            res.end("Shambe");
+
+            const superHero = {
+                firstName: "Bruce",
+                lastName: "Wayne"
+            };
+            res.writeHead(200, {"content-type": "application/json" });
+            res.end(JSON.stringify(superHero));
 })
 
         server.listen(3000, ()=>{
