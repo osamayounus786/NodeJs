da80674092cd0cdfeff6636438ea13c735a0872b
Osama Younus <osama.younus@blended.team>
2023-04-05 13:49:17 +0500

    html response initial

--- index.js
+++ index.js
@@ -131,19 +131,41 @@
 
 //creating node server 
 
+// const http = require("node:http");
+
+// const server =  http.createServer((req,res)=>{
+
+            //JSON Response
+//             const superHero = {
+//                 firstName: "Bruce",
+//                 lastName: "Wayne"
+//             };
+//             res.writeHead(200, {"content-type": "application/json" });
+//             res.end(JSON.stringify(superHero));
+// })
+
+//         server.listen(3000, ()=>{
+//             console.log("Server running on port 3000")
+//         })
+// console.log(http)
+
+
+
+// HTML Responese
+
 const http = require("node:http");
 
 const server =  http.createServer((req,res)=>{
 
+            
             const superHero = {
                 firstName: "Bruce",
                 lastName: "Wayne"
             };
-            res.writeHead(200, {"content-type": "application/json" });
-            res.end(JSON.stringify(superHero));
+            res.writeHead(200, {"Content-Type": "text/html" });
+            res.end("<h1>Hello World</h1>");
 })
 
         server.listen(3000, ()=>{
             console.log("Server running on port 3000")
         })
-// console.log(http)
