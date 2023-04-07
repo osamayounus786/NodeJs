6724cb6d04bc12b0d7d4502d254e07f265b86377
Osama Younus <osama.younus@blended.team>
2023-04-07 12:59:30 +0500

    sameer siddiquie

--- index.js
+++ index.js
@@ -229,6 +229,26 @@ const fs = require("node:fs");
 const server =  http.createServer((req,res)=>{
 
    
+   if(req.url === "/"){
+        res.writeHead(200,{"Content-Type": "text/plain"})
+        res.end("Home Page")
+   }
+   else if(req.url === "/about"){
+    res.writeHead(200,{"Content-Type": "text/plain"})
+    res.end("About Page")
+   }
+   else if(req.url === "/api"){
+    res.writeHead(200,{"Content-Type": "application/JSON"})
+    res.end(JSON.stringify({
+        firstName: "Osama",
+        lastName: "Younus"
+    }))
+   }
+
+   else{
+    res.writeHead(404,{"Content-Type": "text/plain"})
+    res.end("404 Page")
+   }
 })
 
         server.listen(3000, ()=>{
